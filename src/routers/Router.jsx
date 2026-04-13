59166efe85a6d1c241a37f2ffb271c990899cd13
Archita Panda <archita.panda004@gmail.com>
2026-04-13 17:15:57 +0530

    Add Register and AdminLogin routes to Router

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 // import Home from "../pages/Home";
 import Login from "../components/auth/Login";
+import Register from "../components/auth/Register";
+import AdminLogin from "../components/auth/AdminLogin";
 
 const router = createBrowserRouter([
   {
@@ -14,8 +16,15 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/admin-login",
+    element: <AdminLogin />
   }
-
 ]);
 
 export default router;
