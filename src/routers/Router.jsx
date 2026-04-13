9737d92004ac9fda0059d6724eeff5ffbe42992b
Archita Panda <archita.panda004@gmail.com>
2026-04-13 15:39:49 +0530

    Update Router.jsx after login page
    2

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,19 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
+// import Home from "../pages/Home";
+import Login from "../components/auth/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "/", element: <div className="p-4">Home</div> },
-      { path: "/shops", element: <div className="p-4">Shops</div> },
-      { path: "/logout", element: <div className="p-4">Logout</div> },
-      { path: "/account", element: <div>Account</div> },
-      { path: "/settings", element: <div>Settings</div> },
-      { path: "/stats", element: <div>Reading Stats</div> },
+      { path: "/", element: <div className="p-4">Home</div> }
     ],
   },
+  {
+    path: "/login",
+    element: <Login />
+  }
+
 ]);
 
 export default router;
